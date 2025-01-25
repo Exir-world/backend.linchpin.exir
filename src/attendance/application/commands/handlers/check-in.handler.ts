e0b254b91d0247332664e54dc2899ed75a4780cb
erfanemamverdi9235 <erfanemamverdi9235@gmail.com>
2025-01-25 10:22:50 +0330

    fix check in

--- src/attendance/application/commands/handlers/check-in.handler.ts
+++ src/attendance/application/commands/handlers/check-in.handler.ts
@@ -3,6 +3,7 @@ import { CheckInCommand } from "../check-in.command";
 import { AttendanceRepository } from "../../ports/attendance.repository";
 import { Attendance } from "src/attendance/domain/attendance";
 import { BadRequestException } from "@nestjs/common";
+import { DateUtil } from "src/common/utils/date.util";
 
 @CommandHandler(CheckInCommand)
 export class CheckInHandler implements ICommandHandler<CheckInCommand> {
@@ -10,8 +11,14 @@ export class CheckInHandler implements ICommandHandler<CheckInCommand> {
 
     async execute(command: CheckInCommand): Promise<any> {
         const lastAttendance = await this.attendanceRepo.findLastByUserId(command.userId);
-        if (lastAttendance && !lastAttendance.getCheckOut)
-            throw new BadRequestException('You are already checked in!');
+        const todayStart = DateUtil.startOfDay();
+
+        if (lastAttendance) {
+            if (lastAttendance.getCheckIn > todayStart && !lastAttendance.getCheckOut)
+                throw new BadRequestException('You are already checked in!');
+            // else if (lastAttendance.getCheckOut < todayStart && !lastAttendance.getCheckOut)
+            //     throw new BadRequestException('You have already checked out!');
+        }
 
         const attendance = new Attendance(0, command.userId);
         await this.attendanceRepo.save(attendance);
