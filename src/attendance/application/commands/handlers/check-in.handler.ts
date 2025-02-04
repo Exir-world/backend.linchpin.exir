850c41069f4eb5f6cc25d23c85cd10364e9df937
erfanemamverdi9235 <erfanemamverdi9235@gmail.com>
2025-02-04 16:34:42 +0330

    sh

--- src/attendance/application/commands/handlers/check-in.handler.ts
+++ src/attendance/application/commands/handlers/check-in.handler.ts
@@ -26,8 +26,6 @@ export class CheckInHandler implements ICommandHandler<CheckInCommand> {
                 throw new BadRequestException('First submit prevoius time work report!')
         }
 
-
-
         const attendance = new Attendance(0, command.userId);
         const newAttendance = await this.attendanceRepo.save([attendance]);
 
