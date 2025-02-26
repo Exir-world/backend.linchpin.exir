9983d0155deddc47c4662884e26dc2663c97eafb
erfanemamverdi9235 <erfanemamverdi9235@gmail.com>
2025-02-26 12:48:49 +0330

    add cors

--- src/main.ts
+++ src/main.ts
@@ -25,6 +25,42 @@ async function bootstrap() {
 
   setupSwagger(app);
 
+  // cors
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Replace with your frontend origin
+    res.header(
+      'Access-Control-Allow-Methods',
+      'GET,POST,PUT,PATCH,DELETE,OPTIONS',
+    );
+    res.header('Access-Control-Allow-Headers', '*');
+    next();
+  });
+
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://127.0.0.1:3000',
+      'http://127.0.0.1:10809',
+      'http://blackfriday.ex.pro',
+      'https://blackfriday.ex.pro',
+      'http://mystery.ex.pro',
+      'https://mystery.ex.pro',
+      'http://linchpin.ex.pro',
+      'https://linchpin.ex.pro',
+      'http://token.ex.pro',
+      'https://token.ex.pro',
+      'http://crossfunding.ex.pro',
+      'https://crossfunding.ex.pro',
+      'https://7694-31-25-92-200.ngrok-free.app',
+      'http://127.0.0.1:40410',
+      'http://localhost:51664',
+    ], // Replace with your frontend origin
+    // origin: [/\.ex\.pro$/],
+    credentials: true, // Allow cookies to be sent
+    // allowedHeaders: '*',
+    // methods: '*',
+  });
+
   await app.listen(configService.get('PORT') ?? 3000);
 
 }
