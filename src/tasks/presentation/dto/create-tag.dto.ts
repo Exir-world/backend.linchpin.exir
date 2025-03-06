5bd883df0645b621c8088880a131969ec74b5a69
erfanemamverdi9235 <erfanemamverdi9235@gmail.com>
2025-03-06 11:41:05 +0330

    change image urls

--- src/tasks/presentation/dto/create-tag.dto.ts
+++ src/tasks/presentation/dto/create-tag.dto.ts
@@ -17,7 +17,7 @@ export class CreateTagDto {
     @IsNotEmpty()
     textColor: string;
 
-    @ApiProperty({ example: "https://exirtu.be/cdn/linchpin/tag_content_icon.svg", description: "The icon of the tag" })
+    @ApiProperty({ example: "https://cdn.exirtu.be/linchpin/tag_content_icon.svg", description: "The icon of the tag" })
     @IsString()
     @IsNotEmpty()
     icon: string;
