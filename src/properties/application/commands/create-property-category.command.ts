5d1e678a1ca7c6b91213b32e485ce7e8f7148b9c
erfanemamverdi9235 <erfanemamverdi9235@gmail.com>
2025-04-21 13:33:41 +0330

    change features

--- src/properties/application/commands/create-property-category.command.ts
+++ src/properties/application/commands/create-property-category.command.ts
@@ -1,6 +1,6 @@
 export class CreatePropertyCategoryCommand {
     constructor(
         public readonly title: string,
-        public readonly features?: { title: string }[],
+        public readonly features?: string[],
     ) { }
 }
