6b43a6163a6a98ddf342119bc7d4742dfe7eebad
erfanemamverdi9235 <erfanemamverdi9235@gmail.com>
2025-05-29 10:47:44 +0330

    improvement items

--- src/improvement-parameters/presentation/controllers/user-improvement-parameter.controller.ts
+++ src/improvement-parameters/presentation/controllers/user-improvement-parameter.controller.ts
@@ -1,21 +1,24 @@
-import { Controller, Get, Body, Post, Query, Req } from '@nestjs/common';
+import { Controller, Get, Body, Post, Query, Req, UseGuards } from '@nestjs/common';
 import { CommandBus, QueryBus } from '@nestjs/cqrs';
-import { ApiTags, ApiOperation, ApiResponse, ApiQuery } from '@nestjs/swagger';
+import { ApiTags, ApiOperation, ApiResponse, ApiQuery, ApiBearerAuth } from '@nestjs/swagger';
 import { CreateUserImprovementParameterDto } from '../dto/create-user-improvement-parameter.dto';
 import { CreateUserImprovementParameterCommand } from 'src/improvement-parameters/application/commands/create-user-improvement-parameter.command';
 import { GetUserImprovementParametersQuery } from 'src/improvement-parameters/application/queries/get-user-improvement-parameters.query';
+import { UserAuthGuard } from 'src/auth/application/guards/user-auth.guard';
 
+@ApiBearerAuth()
 @ApiTags('User Improvement Parameters')
 @Controller('user-improvement-parameters')
 export class UserImprovementParameterController {
     constructor(private readonly commandBus: CommandBus, private readonly queryBus: QueryBus) { }
 
+    @UseGuards(UserAuthGuard) // Assuming you have a JWT Auth Guard
     @Post()
     @ApiOperation({ summary: 'ایجاد پارامترهای بهبود برای کاربر' })
     @ApiResponse({ status: 201, description: 'ایجاد موفق پارامترهای کاربر' })
     @ApiResponse({ status: 400, description: 'Validation failed' })
     async createUserImprovementParams(@Req() req, @Body() body: CreateUserImprovementParameterDto) {
-        const userId = 1;
+        const userId = req.user.id;
         const parentId = await this.commandBus.execute(
             new CreateUserImprovementParameterCommand(
                 userId,
@@ -28,12 +31,13 @@ export class UserImprovementParameterController {
         return this.queryBus.execute(new GetUserImprovementParametersQuery(userId, parentId));
     }
 
+    @UseGuards(UserAuthGuard) // Assuming you have a JWT Auth Guard
     @Get()
     @ApiOperation({ summary: '' })
     @ApiResponse({ status: 200 })
     @ApiQuery({ name: 'parentId', required: false, type: Number })
     async getUserImprovementParams(@Req() req, @Query('parentId') parentId?: number) {
-        const userId = 1;
+        const userId = req.user.id;
         return this.queryBus.execute(new GetUserImprovementParametersQuery(userId, parentId));
     }
 }
